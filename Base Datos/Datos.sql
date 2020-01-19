681968d923f31407d4f2768d771f5d3e1d935ce5
Josue Davalos <jedavalo@espol.edu.ec>
2020-01-19 00:38:02 -0500

    Base Datos inicial

---
+++ Base Datos/Datos.sql
@@ -0,0 +1,137 @@
+use dr_pelos;
+
+insert into JefeBodega(cedula, nombre, apellido, genero) values ("0931657365","Bryan","Davalos","M");
+insert into JefeBodega(cedula, nombre, apellido, genero) values ("0958745698","Josue","Sanchez","M");
+insert into JefeBodega(cedula, nombre, apellido, genero) values ("0956985889","Erick","Vaca","M");
+insert into JefeBodega(cedula, nombre, apellido, genero) values ("0956592330","Eunice","Gorotiza","F");
+insert into JefeBodega(cedula, nombre, apellido, genero) values ("0905899128","Carlos","Vazquez","M");
+insert into JefeBodega(cedula, nombre, apellido, genero) values ("0296999941","Xiomara","Mera","F");
+insert into JefeBodega(cedula, nombre, apellido, genero) values ("0795989541","Jocellyne","Luna","F");
+
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("0931657365","Bryan","Sanchez","1999-12-18","M","brymsanc@espol.edu.ec","Acuarela del rio Mz 2 V1","0989195454");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("7894561230","Mois","Cabezas","1999-12-25","M","bmsa@espol.edu.ec","Sauces","0989845621");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("1234560785","Josue","Davalos","1990-01-02","M","josuetunene@gmail.com","Acuarela del rio Mz 2 V1","0935698745");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("7412589630","Galrer","Saltos","1975-01-18","M","gallarota@gmail.com","Sauces 9 V1","0956471236");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("9632587410","Ginger","Saltos","1960-02-04","F","gsaltos@espol.edu.ec","Ceibos","0956847125");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("8521479630","Eduardo","Cruz","1975-05-08","M","ecruz@espol.edu.ec","Entrada de la 8","0965748954");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("0254136987","Allan","Galarraga","1990-04-05","M","elgala@espol.edu.ec","Acuarela del rio Mz 2 V1","0984571659");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("8544961234","Kimberly","Suarez","1999-12-18","F","kimberlyprexiositaemoxitap@espol.edu.ec","Aquisito","0945687521");
+insert into Cliente(cedula, nombre, apellido, FechaNacimiento, genero, correo, direccion, telefono) Values ("0931655365","Jefferson","Sanchez","1999-11-18","M","jeffer@espol.edu.ec","Mapasingue Oeste","0954645756");
+
+insert into Producto(nombre, categoria, precio) values ("Pelota","Juguete",10.55);
+insert into Producto(nombre, categoria, precio) values ("Hueso","Juguete",11.85);
+insert into Producto(nombre, categoria, precio) values ("Camisa Avenger","Ropa",10.99);
+insert into Producto(nombre, categoria, precio) values ("Lazo","Ropa",1.10);
+insert into Producto(nombre, categoria, precio) values ("Antimoquillo","Medina",4.50);
+insert into Producto(nombre, categoria, precio) values ("Paracetamol","Medina",4.55);
+insert into Producto(nombre, categoria, precio) values ("Cereal grande","Alimento",14.15);
+insert into Producto(nombre, categoria, precio) values ("Hueso comestible","Alimento",2.50);
+insert into Producto(nombre, categoria, precio) values ("Pelota saltarina","Juguete",1.00);
+insert into Producto(nombre, categoria, precio) values ("Cereal pequeño","Alimento",5.55);
+
+insert into sucursal(direccion) values ("Sauce 2 C 3era. Av. 8va.");
+insert into sucursal(direccion) values ("Ceibos cuidadela Cumbres");
+insert into sucursal(direccion) values ("Mapsingue Oeste, 5 1/9 km Via daule");
+insert into sucursal(direccion) values ("Centro, boyaca y aguirre");
+insert into sucursal(direccion) values ("Villa España");
+insert into sucursal(direccion) values ("La joya");
+
+insert into Repartidor(cedula, nombre, apellido, genero) values ("0931548765","Robert","Mera","M");
+insert into Repartidor(cedula, nombre, apellido, genero) values ("0995648549","Domenica","Ochoa","F");
+insert into Repartidor(cedula, nombre, apellido, genero) values ("0950095979","Xavier","Bermudez","M");
+insert into Repartidor(cedula, nombre, apellido, genero) values ("0129997415","Carlos","Medina","M");
+insert into Repartidor(cedula, nombre, apellido, genero) values ("0895659594","Dustin","Quinde","M");
+insert into Repartidor(cedula, nombre, apellido, genero) values ("0987876598","Milena","suarez","F");
+insert into Repartidor(cedula, nombre, apellido, genero) values ("0598985655","Melany","Cedeño","F");
+
+insert into nCredito(monto, descripcion) values (45.45, "descripcion 1");
+insert into nCredito(monto, descripcion) values (100.36, "descripcion 2");
+insert into nCredito(monto, descripcion) values (12.65, "descripcion 3");
+insert into nCredito(monto, descripcion) values (485.66, "descripcion 4");
+insert into nCredito(monto, descripcion) values (153.55, "descripcion 5");
+insert into nCredito(monto, descripcion) values (40.56, "descripcion 6");
+insert into nCredito(monto, descripcion) values (89.23, "descripcion 7");
+
+insert into Factura(monto, descripcion) values (89.23, "descripcion 7");
+insert into Factura(monto, descripcion) values (45.45, "descripcion 1");
+insert into Factura(monto, descripcion) values (100.36, "descripcion 2");
+insert into Factura(monto, descripcion) values (12.65, "descripcion 3");
+insert into Factura(monto, descripcion) values (485.66, "descripcion 4");
+insert into Factura(monto, descripcion) values (153.55, "descripcion 5");
+insert into Factura(monto, descripcion) values (40.56, "descripcion 6");
+insert into Factura(monto, descripcion) values (89.23, "descripcion 7");
+
+insert into cotizacion(monto, descripcion) values (89.23, "descripcion 7");
+insert into cotizacion(monto, descripcion) values (45.45, "descripcion 1");
+insert into cotizacion(monto, descripcion) values (100.36, "descripcion 2");
+insert into cotizacion(monto, descripcion) values (12.65, "descripcion 3");
+insert into cotizacion(monto, descripcion) values (485.66, "descripcion 4");
+insert into cotizacion(monto, descripcion) values (153.55, "descripcion 5");
+insert into cotizacion(monto, descripcion) values (40.56, "descripcion 6");
+insert into cotizacion(monto, descripcion) values (89.23, "descripcion 7");
+
+insert into FormaPago(factura,cotizacion) values (1,1);
+insert into FormaPago(factura,cotizacion) values (2,2);
+insert into FormaPago(factura,cotizacion) values (3,2);
+insert into FormaPago(factura,cotizacion) values (4,3);
+insert into FormaPago(factura,cotizacion) values (5,1);
+insert into FormaPago(factura,cotizacion) values (6,2);
+insert into FormaPago(factura,cotizacion) values (7,2);
+insert into FormaPago(nCredito,cotizacion) values (1,1);
+insert into FormaPago(nCredito,cotizacion) values (2,2);
+insert into FormaPago(nCredito,cotizacion) values (3,4);
+insert into FormaPago(nCredito,cotizacion) values (4,3);
+insert into FormaPago(nCredito,cotizacion) values (5,6);
+insert into FormaPago(nCredito,cotizacion) values (6,5);
+
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 1",1,1);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 2",2,3);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 3",3,2);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 4",4,2);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 5",6,1);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 6",5,7);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 7",7,6);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 8",1,5);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 9",2,4);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 10",4,3);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 11",6,2);
+insert into DocumentoEntrega(descripcion,repartidor,jefeBodega) values ("Entrega del pedido 12",3,1);
+
+insert into DetallePago(repartidor,cantidad) values (3,11.11);
+insert into DetallePago(repartidor,cantidad) values (4,659.21);
+insert into DetallePago(repartidor,cantidad) values (5,12.36);
+insert into DetallePago(repartidor,cantidad) values (6,15.65);
+insert into DetallePago(repartidor,cantidad) values (1,55.66);
+insert into DetallePago(repartidor,cantidad) values (2,102.36);
+insert into DetallePago(repartidor,cantidad) values (3,123.36);
+insert into DetallePago(repartidor,cantidad) values (4,125.66);
+insert into DetallePago(repartidor,cantidad) values (5,145.88);
+
+insert into ProductoSucursal(producto,sucursal) values (1,1);
+insert into ProductoSucursal(producto,sucursal) values (2,2);
+insert into ProductoSucursal(producto,sucursal) values (3,2);
+insert into ProductoSucursal(producto,sucursal) values (4,3);
+insert into ProductoSucursal(producto,sucursal) values (5,1);
+insert into ProductoSucursal(producto,sucursal) values (6,2);
+insert into ProductoSucursal(producto,sucursal) values (7,2);
+insert into ProductoSucursal(producto,sucursal) values (1,1);
+insert into ProductoSucursal(producto,sucursal) values (2,2);
+insert into ProductoSucursal(producto,sucursal) values (3,4);
+insert into ProductoSucursal(producto,sucursal) values (4,3);
+insert into ProductoSucursal(producto,sucursal) values (5,6);
+insert into ProductoSucursal(producto,sucursal) values (6,5);
+
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (6,7,1,3,1,"2019-12-12");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (1,6,1,5,2,"2019-12-13");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (2,5,3,4,3,"2019-12-12");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (3,4,4,3,4,"2019-12-12");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (4,3,5,2,4,"2019-12-14");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (5,2,6,1,6,"2019-12-15");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (6,1,7,7,5,"2019-12-16");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (7,7,1,6,6,"2019-12-16");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (7,6,1,5,7,"2019-12-17");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (1,5,2,3,1,"2019-12-18");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (2,4,3,4,2,"2019-12-16");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (3,3,3,2,3,"2019-12-12");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (5,3,1,4,4,"2019-12-13");
+insert into Pago(producto,cliente,sucursal,formaPago,detallePago,fecha) values (4,1,2,1,5,"2019-12-12");
---
+++ Base Datos/Tablas.sql
@@ -0,0 +1,180 @@
+drop database if exists dr_pelos;
+
+Create database dr_pelos;
+USE dr_pelos ;
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`JefeBodega`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.JefeBodega (
+  ID INT NOT NULL AUTO_INCREMENT,
+  cedula VARCHAR(10) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  genero char(1)  NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`Cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.Cliente (
+  ID INT NOT NULL AUTO_INCREMENT,
+  cedula VARCHAR(10) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  genero char(1)  NOT NULL,
+  correo varchar(50) not null,
+  FechaNacimiento DATE NOT NULL,
+  direccion VARCHAR(50) NOT NULL,
+  telefono VARCHAR(15) NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`Producto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.Producto (
+  ID INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  categoria VARCHAR(50) NOT NULL,
+  precio double not null,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`Servicio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.Servicio (
+  ID INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(50) NOT NULL,
+  categoria VARCHAR(50) NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`Sucursal`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.Sucursal (
+  ID INT NOT NULL AUTO_INCREMENT,
+  direccion VARCHAR(50) NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`Repartidor`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.Repartidor (
+  ID INT NOT NULL AUTO_INCREMENT,
+  cedula VARCHAR(10) NOT NULL,
+  nombre VARCHAR(50) NOT NULL,
+  apellido VARCHAR(50) NOT NULL,
+  genero char(1)  NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`nCredito`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.nCredito (
+  ID INT NOT NULL AUTO_INCREMENT,
+  monto double NOT NULL,
+  descripcion VARCHAR(50) NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`Factura`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.Factura (
+  ID INT NOT NULL AUTO_INCREMENT,
+  monto double NOT NULL,
+  descripcion VARCHAR(50) NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`cotizacion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.cotizacion (
+  ID INT NOT NULL AUTO_INCREMENT,
+  monto double NOT NULL,
+  descripcion VARCHAR(50) NOT NULL,
+  PRIMARY KEY (ID))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`FormaPago`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.FormaPago (
+  ID INT NOT NULL AUTO_INCREMENT,
+  factura INT,
+  ncredito INT,
+  cotizacion INT,
+  PRIMARY KEY (ID),
+  foreign key (factura) references dr_pelos.factura (ID),
+  foreign key (ncredito) references dr_pelos.ncredito (ID),
+  foreign key (cotizacion) references dr_pelos.cotizacion (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`DocumentoEntrega`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.DocumentoEntrega (
+  ID INT NOT NULL AUTO_INCREMENT,
+  descripcion VARCHAR(50) NOT NULL,
+  repartidor INT NOT NULL,
+  jefeBodega INT NOT NULL,
+  PRIMARY KEY (ID),
+  foreign key (repartidor) references dr_pelos.Repartidor (ID),
+  foreign key (jefeBodega) references dr_pelos.JefeBodega (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`DetallePago`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.DetallePago (
+  ID INT NOT NULL AUTO_INCREMENT,
+  repartidor INT NOT NULL,
+  cantidad double NOT NULL,
+  PRIMARY KEY (ID),
+  foreign key (repartidor) references dr_pelos.Repartidor (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`ProductoSucursal`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.ProductoSucursal (
+  producto INT NOT NULL,
+  sucursal INT NOT NULL,
+  foreign key (producto) references dr_pelos.producto (ID),
+  foreign key (sucursal) references dr_pelos.sucursal (ID))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `dr_pelos`.`Pago`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS dr_pelos.Pago (
+  producto INT NOT NULL,
+  cliente INT NOT NULL,
+  fecha DATE NOT NULL,
+  sucursal INT NOT NULL,
+  formaPago INT NOT NULL,
+  detallePago INT NOT NULL,
+  foreign key (producto) references dr_pelos.producto (ID),
+  foreign key (cliente) references dr_pelos.cliente (ID),
+  foreign key (formaPago) references dr_pelos.formaPago (ID),
+  foreign key (detallePago) references dr_pelos.detallePago (ID))
+ENGINE = InnoDB;
+
+
+
